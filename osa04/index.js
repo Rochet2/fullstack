5c1e206e26711242fdd62ec1b2bda832ec3a1bfb
Rochet2 <tqbattlenet@gmail.com>
2018-02-03 18:10:24 +0200

    Flesh out the app

--- osa04/index.js
+++ osa04/index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const Blog = require('./models/blog')
 
 const app = express()
 
@@ -15,7 +16,23 @@ morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 app.use(bodyParser.json())
 
-// routes here
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
